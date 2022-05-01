293b7d6ca6b5083c6e762c57e1ae5c0d14f12668
Hammad Asghar <ihammadasghar@gmail.com>
2022-05-01 15:56:49 +0100

    Organize code better in cli

--- manutrix/cli.py
+++ manutrix/cli.py
@@ -4,15 +4,16 @@ import colorama
 from colorama import Fore
 colorama.init(autoreset=True, convert=True)
 
+#  Session data
+matrixes = {}
 
 def main():
     intro()
-    #  Session data
-    matrixes = {}
     
     while True:
         commands = input().split(" ")
         command = commands[0].upper()
+        arguments = commands[1:]
 
         if command == "EXIT":
             break
@@ -21,65 +22,34 @@ def main():
             intro()
 
         elif command == "MATRIX":
-            if correct_args(commands, 1):
-                name = commands[1]
-                dims = input(f"{Fore.BLUE}Dimensions (e.g 3x3): ")
-                rows, cols = map(int, dims.split("x"))
-                new_matrix = matrix()
-
-                i = 0
-                while i < rows:
-                    row = input(f"{Fore.BLUE}Row {i+1}: ")
-                    row = list(map(float, row.split(" ")))
-                    if len(row) != cols:
-                        print(f"{Fore.RED}{cols} elements required in each row.")
-                        continue
-                    new_matrix.add_row(row)
-                    i +=1
-                    
+            if correct_args(commands, 2):
+                make_matrix(*arguments)
                 
-                matrixes[name] = new_matrix
-                print(f"{Fore.GREEN}Matrix {name} added.")
         
         elif command == "RANDOM":
             if correct_args(commands, 1):
-                name = commands[1]
-                dims = input(f"{Fore.BLUE}Dimensions (e.g 3x3): ")
-                rows, cols = map(int, dims.split("x"))
-                new_matrix = matrix()
-                new_matrix.random(rows, cols)
-                matrixes[name] = new_matrix
-                print(f"{Fore.GREEN}Matrix {name} added.")
+                make_random_matrix(*arguments)
         
         elif command == "SHOW":
             if correct_args(commands, 1):
-                name = commands[1]
-                print(f"{Fore.BLUEls}Matrix {name} {matrixes[name].get_dims()}:\n")
-                print_matrix(matrixes[name].matrix)
+                show_matrix(*arguments)
+                
 
         elif command == "MULT":
             if correct_args(commands, 2):
-                a, b = commands[1:]
-                result = matrixes[a].multiply(matrixes[b])
-                print_matrix(result)
+                multiply_matrix(*arguments)
 
         elif command == "ADD":
-            if correct_args(commands, 2):
-                a, b = commands[1:]
-                result = matrixes[a].add(matrixes[b])
-                print_matrix(result)
+            if correct_args(commands, 3):
+                add_matrix(*arguments)
 
         elif command == "SUB":
             if correct_args(commands, 2):
-                a, b = commands[1:]
-                result = matrixes[a].subtract(matrixes[b])
-                print_matrix(result)
+                subtract_matrix(*arguments)
 
         elif command == "SCALE":
             if correct_args(commands, 2):
-                n, name = commands[1:]
-                result = matrixes[name].scale(int(n))
-                print_matrix(result)
+                scale_matrix(*arguments)
         
         elif command == "ADDROW":
             pass
@@ -112,4 +82,54 @@ def correct_args(commands, n):
     print(f"{Fore.RED}Required number of arguments is {n}")
     return False
                 
+
+def make_matrix(name, dims):
+    rows, cols = map(int, dims.split("x"))
+    new_matrix = matrix()
+
+    i = 0
+    while i < rows:
+        row = input(f"{Fore.BLUE}Row {i+1}: ")
+        row = list(map(float, row.split(" ")))
+        if len(row) != cols:
+            print(f"{Fore.RED}{cols} elements required in each row.")
+            continue
+        new_matrix.add_row(row)
+        i +=1
+        
+    matrixes[name] = new_matrix
+    print(f"{Fore.GREEN}Matrix {name} added.")
+
+
+def make_random_matrix(name, dims):
+    rows, cols = map(int, dims.split("x"))
+    new_matrix = matrix()
+    new_matrix.random(rows, cols)
+    matrixes[name] = new_matrix
+    print(f"{Fore.GREEN}Matrix {name} added.")
+
+
+def show_matrix(name):
+    print(f"{Fore.BLUEls}Matrix {name} {matrixes[name].get_dims()}:\n")
+    print_matrix(matrixes[name].matrix)
+
+
+def multiply_matrix(a, b):
+    result = matrixes[a].multiply(matrixes[b])
+    print_matrix(result)
+
+
+def add_matrix(a, b):
+    result = matrixes[a].add(matrixes[b])
+    print_matrix(result)
+
+
+def subtract_matrix(a, b):
+    result = matrixes[a].subtract(matrixes[b])
+    print_matrix(result)
+
+
+def scale_matrix(name, n):
+    result = matrixes[name].scale(int(n))
+    print_matrix(result)
             
